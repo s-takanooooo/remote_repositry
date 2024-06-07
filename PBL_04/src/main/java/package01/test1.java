b089fa03881002096481392d5a4ec21616bc5cd5
User <User@WINDOWS-2LH1NS7>
2024-06-07 13:45:31 +0900

    test(by大石航馬)

--- PBL_04/src/main/java/package01/test1.java
+++ PBL_04/src/main/java/package01/test1.java
@@ -1,7 +1,8 @@
 package package01;
 
 public class test1 {
-public static void main(String[] args) {
-	System.out.println("saebetakano");
-}
+	public static void main(String[] args) {
+		System.out.println("saebetakano");
+		System.out.println("大石航馬");
+	}
 }
