cfca3a681908b2fd6ed8bc0eb5281d89d7d99aa0
User <User@WINDOWS-9201UDC>
2024-06-10 16:56:37 +0900

    Loginメソッドの追加

--- PBL_04/src/main/java/services/AccountsServices.java
+++ PBL_04/src/main/java/services/AccountsServices.java
@@ -1,9 +1,33 @@
 package services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import util.DbUtil;
+
 public class AccountsServices {
 
-	public void Login(String account_name, String mail) {
+	public boolean Login(String account_name, String mail) throws Exception{
+		
+		
 		String sql = "SELECT * FROM accounts WHERE account_name = ? AND mail = ?";
 
+		try (
+				Connection conn = DbUtil.open();
+				PreparedStatement ps = conn.prepareStatement(sql);) {
+
+			ps.setString(1, account_name);
+			ps.setString(2, mail);
+
+			ResultSet rs = ps.executeQuery();
+
+			if(rs != null) {
+				return true;  
+			}else {
+				return false;
+			}
+			
+		} 
 	}
 }
