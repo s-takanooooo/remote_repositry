e944055e8f93980cbf7752d7cd1567c6190c3625
User <User@WINDOWS-I8AQHM6>
2024-06-10 16:04:40 +0900

    sales

--- PBL_04/src/main/java/controller/SalesRegister.java
+++ PBL_04/src/main/java/controller/SalesRegister.java
@@ -11,14 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class salseRegisterServlet
  */
-@WebServlet("/salseRegisterServlet")
-public class salseRegisterServlet extends HttpServlet {
+@WebServlet("/SalesRegister")
+public class SalesRegister extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public salseRegisterServlet() {
+    public SalesRegister() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,7 +28,7 @@ public class salseRegisterServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		this.getServletContext().getRequestDispatcher("/salseRegister.jsp").forward(request, response);
+		this.getServletContext().getRequestDispatcher("/salesRegister.jsp").forward(request, response);
 	}
 
 	/**
