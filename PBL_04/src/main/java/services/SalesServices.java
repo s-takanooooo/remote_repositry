9943424b54aed5df0457686dd8d19bb4ef78d130
User <User@WINDOWS-2LH1NS7>
2024-06-11 12:06:09 +0900

    コメント追加

--- PBL_04/src/main/java/services/SalesServices.java
+++ PBL_04/src/main/java/services/SalesServices.java
@@ -31,22 +31,32 @@ public class SalesServices {
 		return cb.getCategory_id();
 	}
 	
-	
 	public void salesInsert(String day, int account_id,String sales_category,
 			String trade_name,String unit_price,String sales_num,
 			String sales_note) {
+		//sql文作成
 		String sql ="INSERT INTO sales (sale_date,account_id,category_id,trade_name,unit_price,sale_number,note)VALUES (?,?,?,?,?,?,?)";
 		try(
+				//db接続
 				Connection con =DbUtil.open();
+				//sql文送信
 				PreparedStatement stmt=con.prepareStatement(sql);
 				){
+			//1個目のに値をセット
 			stmt.setDate(1,  Date.valueOf(day));
+			//2個目のに値をセット
 			stmt.setInt(2, account_id);
+			//3個目のに値をセット
 			stmt.setInt(3, this.getCategoryId(sales_category));
+			//4個目のに値をセット
 			stmt.setString(4, trade_name);
+			//5個目のに値をセット
 			stmt.setInt(5, Integer.parseInt(unit_price));
+			//6個目のに値をセット
 			stmt.setInt(6, Integer.parseInt(sales_num));
+			//7個目のに値をセット
 			stmt.setString(7,sales_note);
+			//sql文実行
 			int result = stmt.executeUpdate();
 			System.out.println(result);
 		}catch (SQLException e) {
