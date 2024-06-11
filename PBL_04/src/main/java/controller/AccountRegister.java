f7d6ce4078f510367be912ffb760133e1fe24bef
User <User@WINDOWS-9201UDC>
2024-06-11 16:14:42 +0900

    アカウント登録機能の修正

--- PBL_04/src/main/java/controller/AccountRegister.java
+++ PBL_04/src/main/java/controller/AccountRegister.java
@@ -53,19 +53,10 @@ public class AccountRegister extends HttpServlet {
 		request.setAttribute("permission", permission);
 		request.setAttribute("passConfirm", passConfirm);
 		
-		if(name.length() >= 20) {
-			errorMessage = "名前は30文字以内で入力してください";
-		}
-		if(mail.length() <= 100) {
-			errorMessage = "メールアドレスは100文字以内で入力してください";
-		}
-		if(pass.length() <= 30) {
-			errorMessage = "パスワードは30文字以内で入力してください";
-		}else if(!passConfirm.equals(pass)) {
+		if(!passConfirm.equals(pass)) {
 			boolean f = false;
 			request.setAttribute("same", f);
 			request.getRequestDispatcher("/accountRegister.jsp").forward(request, response);
-			
 		} else {
 			this.getServletContext().getRequestDispatcher("/accountRegisterConfirm.jsp").forward(request, response);
 		}
