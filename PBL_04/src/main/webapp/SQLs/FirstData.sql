95b600c365e1dab6bd829cfd3829aa52bea0265e
User <User@WINDOWS-2LH1NS7>
2024-06-11 10:24:06 +0900

    categories、alter文insert文

--- PBL_04/src/main/webapp/SQLs/FirstData.sql
+++ PBL_04/src/main/webapp/SQLs/FirstData.sql
@@ -1,1 +1,6 @@
 INSERT INTO accounts (name, mail,password, authority) VALUES ("山田太郎", "taro_yamada@tomatosystem.co.jp", "Taro1234", 1);
+//category_idをAUTO_INCREMENT
+ALTER TABLE categories MODIFY COLUMN category_id INT AUTO_INCREMENT;
+//データinsert
+INSERT INTO categories (category_name) VALUES("食料品");
+INSERT INTO categories (category_name) VALUES("日用品");
