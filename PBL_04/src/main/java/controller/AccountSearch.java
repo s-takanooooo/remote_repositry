214e9628acc8c881291e82faafdcf8b64321151b
User <User@WINDOWS-I8AQHM6>
2024-06-11 09:05:15 +0900

    search

---
+++ PBL_04/src/main/java/controller/AccountSearch.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AccountSearch
+ */
+@WebServlet("/AccountSearch")
+public class AccountSearch extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AccountSearch() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.getServletContext().getRequestDispatcher("/accountSearch.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.getServletContext().getRequestDispatcher("/accountSearchResult.jsp").forward(request, response);
+	}
+
+}
