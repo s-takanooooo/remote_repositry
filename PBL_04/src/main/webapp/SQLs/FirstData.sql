e08297a6eaed5d13bb1ebd370be8a9d03c778081
User <User@WINDOWS-2LH1NS7>
2024-06-11 14:19:19 +0900

    insert文追加

--- PBL_04/src/main/webapp/SQLs/FirstData.sql
+++ PBL_04/src/main/webapp/SQLs/FirstData.sql
@@ -6,3 +6,5 @@ INSERT INTO categories (category_name) VALUES("食料品");
 INSERT INTO categories (category_name) VALUES("日用品");
 //sale_idをAUTO_INCREMENT
 ALTER TABLE sales MODIFY COLUMN sale_id INT AUTO_INCREMENT;
+//sales	のデータinsert
+INSERT INTO sales (sale_date,account_id,category_id,trade_name,unit_price,sale_number,note) VALUES("2024-06-11",1,1,"りんご",100,1000,"青森県産");
