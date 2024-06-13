203cc6a5bee578872ed1ed1960c5e9a84fe4f35f
User <User@WINDOWS-I8AQHM6>
2024-06-13 17:21:43 +0900

    servlet

--- PBL_04/src/main/java/controller/SalesSearch.java
+++ PBL_04/src/main/java/controller/SalesSearch.java
@@ -7,7 +7,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import services.SalesServices;
 
@@ -45,20 +44,7 @@ public class SalesSearch extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.setCharacterEncoding("utf8");
-
-		String min_day = request.getParameter("startDate");
-		String max_day = request.getParameter("endDate");
-		String name = request.getParameter("staff");
-		String sale_category = request.getParameter("category");
-		String trade_name = request.getParameter("mail");
-		String sale_note = request.getParameter("note");
-
-		SalesServices si = new SalesServices();
-		HttpSession session = request.getSession();
-		session.setAttribute("sales", si.SearchSales(min_day, max_day, name, sale_category, trade_name, sale_note));
-
-		this.getServletContext().getRequestDispatcher("/salesSearchAll.jsp").forward(request, response);
+		
 	}
 
 }
