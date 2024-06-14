fda12891569b96b467303f4556edee43fbd1a2b8
User <User@WINDOWS-I8AQHM6>
2024-06-14 13:17:43 +0900

    register

--- PBL_04/src/main/java/controller/SalesRegister.java
+++ PBL_04/src/main/java/controller/SalesRegister.java
@@ -16,21 +16,22 @@ import services.SalesServices;
 @WebServlet("/SalesRegister")
 public class SalesRegister extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SalesRegister() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	SalesServices ss = new SalesServices();
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SalesRegister() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		SalesServices ss = new SalesServices();
+		
 		//ss.getCatgoryName()を呼んだらcategory_nameが入った配列がvlaueにセットされる
 		request.setAttribute("categories", ss.getCatgoryName());
 		request.setAttribute("name", ss.getStaffName());
@@ -40,11 +41,12 @@ public class SalesRegister extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
 		request.setCharacterEncoding("utf8");
-		
+
 		String day = request.getParameter("sale");
 		String responsible = request.getParameter("responsible");
 		String sales_category = request.getParameter("category");
@@ -52,24 +54,46 @@ public class SalesRegister extends HttpServlet {
 		String unit_price = request.getParameter("unit_price");
 		String sales_num = request.getParameter("sale_number");
 		String sales_note = request.getParameter("note");
-		
-		
+
 		int price = Integer.parseInt(unit_price);
 		int num = Integer.parseInt(sales_num);
-		
-		int subtotal = price * num;
-		
-		request.setAttribute("day", day);
-		request.setAttribute("responsible", responsible);
-		request.setAttribute("sales_category", sales_category);
-		request.setAttribute("trade_name", trade_name);
-		request.setAttribute("unit_price", unit_price);
-		request.setAttribute("sales_num", sales_num);
-		request.setAttribute("sales_note", sales_note);
-		request.setAttribute("subtotal", subtotal);
 
-		
-		this.getServletContext().getRequestDispatcher("/salesRegisterConfirm.jsp").forward(request, response);
+		long subtotal = price * num;
+		//バリデーション
+		boolean f = true;
+		if (ss.selectCheck(responsible) == false) {
+			f = false;
+			request.setAttribute("staffError", f);
+		}
+		if (ss.selectCheck(sales_category) == false) {
+			f = false;
+			request.setAttribute("categoryNameError", f);
+		}
+		if (ss.salePriceCheck(unit_price) == false) {
+			f = false;
+			request.setAttribute("unitPriceError", f);
+		}
+		if (ss.saleNumCheck(sales_num) == false) {
+			f = false;
+			request.setAttribute("saleNumError", f);
+		}
+		if (ss.noteCheck(sales_note) == false) {
+			f = false;
+			request.setAttribute("noteError", f);
+		}
+		if (f == false) {
+			doGet(request, response);
+		} else {
+			request.setAttribute("day", day);
+			request.setAttribute("responsible", responsible);
+			request.setAttribute("sales_category", sales_category);
+			request.setAttribute("trade_name", trade_name);
+			request.setAttribute("unit_price", unit_price);
+			request.setAttribute("sales_num", sales_num);
+			request.setAttribute("sales_note", sales_note);
+			request.setAttribute("subtotal", subtotal);
+			this.getServletContext().getRequestDispatcher("/salesRegisterConfirm.jsp").forward(request, response);
+		}
 	}
 
 }
