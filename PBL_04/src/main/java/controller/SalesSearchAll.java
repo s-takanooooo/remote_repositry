6cd4d887bfd22deaf9ad83d6ea1ab33928befd2c
User <User@WINDOWS-I8AQHM6>
2024-06-14 09:14:23 +0900

    salesSearchAllServlet作りました

---
+++ PBL_04/src/main/java/controller/SalesSearchAll.java
@@ -0,0 +1,76 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import services.SalesServices;
+
+/**
+ * Servlet implementation class SalesSearchAll
+ */
+@WebServlet("/SalesSearchAll")
+public class SalesSearchAll extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SalesSearchAll() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		SalesServices ss = new SalesServices();
+		HttpSession session = request.getSession();
+		String min_day = (String) session.getAttribute("min_day");
+		String max_day = (String) session.getAttribute("max_day");
+		String name = (String) session.getAttribute("name");
+		String sale_category = (String) session.getAttribute("sale_category");
+		String trade_name = (String) session.getAttribute("trade_name");
+		String sale_note = (String) session.getAttribute("sale_note");
+		session.setAttribute("sales", ss.searchSales(min_day, max_day, name, sale_category, trade_name, sale_note));
+
+		this.getServletContext().getRequestDispatcher("/salesSearchAll.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf8");
+
+		String min_day = request.getParameter("startDate");
+		String max_day = request.getParameter("endDate");
+		String name = request.getParameter("staff");
+		String sale_category = request.getParameter("category");
+		String trade_name = request.getParameter("mail");
+		String sale_note = request.getParameter("note");
+
+		SalesServices si = new SalesServices();
+		HttpSession session = request.getSession();
+		session.setAttribute("min_day", min_day);
+		session.setAttribute("max_day", max_day);
+		session.setAttribute("name", name);
+		session.setAttribute("sale_category", sale_category);
+		session.setAttribute("trade_name", trade_name);
+		session.setAttribute("sale_note", sale_note);
+		session.setAttribute("sales", si.searchSales(min_day, max_day, name, sale_category, trade_name, sale_note));
+
+		this.getServletContext().getRequestDispatcher("/salesSearchAll.jsp").forward(request, response);
+	}
+
+}
