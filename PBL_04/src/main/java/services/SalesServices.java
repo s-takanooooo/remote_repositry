aff2eef7cc15cf48a91fadc845431edb1e6ea68c
User <User@WINDOWS-2LH1NS7>
2024-06-19 15:01:23 +0900

    subtotalをBigIntegerに変更

--- PBL_04/src/main/java/services/SalesServices.java
+++ PBL_04/src/main/java/services/SalesServices.java
@@ -15,54 +15,51 @@ import util.DbUtil;
 
 public class SalesServices {
 	public int getCategoryId(String catrgory_name) {
-		String sql ="SELECT * from categories WHERE category_name=?";
-		CategoriesBean cb =null;
-		try(
-				Connection con =DbUtil.open();
-				PreparedStatement stmt=con.prepareStatement(sql);
-				){
-			stmt.setString(1,catrgory_name);
+		String sql = "SELECT * from categories WHERE category_name=?";
+		CategoriesBean cb = null;
+		try (
+				Connection con = DbUtil.open();
+				PreparedStatement stmt = con.prepareStatement(sql);) {
+			stmt.setString(1, catrgory_name);
 			ResultSet rs = stmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				cb = new CategoriesBean(
 						rs.getInt("category_id"),
 						rs.getString("category_name"),
 						rs.getInt("active_flg"));
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return cb.getCategory_id();
 	}
-	
-	public ArrayList<String> getCatgoryName(){
-		String sql= "SELECT category_name FROM categories";
+
+	public ArrayList<String> getCatgoryName() {
+		String sql = "SELECT category_name FROM categories";
 		ArrayList<String> categories = new ArrayList<>();
-		try(
-				Connection con =DbUtil.open();
-				PreparedStatement stmt = con.prepareStatement(sql);
-				){
+		try (
+				Connection con = DbUtil.open();
+				PreparedStatement stmt = con.prepareStatement(sql);) {
 			ResultSet rs = stmt.executeQuery();
-			while(rs.next()) {
-				String category =rs.getString("category_name");
+			while (rs.next()) {
+				String category = rs.getString("category_name");
 				categories.add(category);
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return categories;
 	}
-	
+
 	public int getAccountId(String account_name) {
-		String sql ="SELECT * from accounts WHERE name=?";
-		AccountsBean ab =null;
-		try(
-				Connection con =DbUtil.open();
-				PreparedStatement stmt=con.prepareStatement(sql);
-				){
-			stmt.setString(1,account_name);
+		String sql = "SELECT * from accounts WHERE name=?";
+		AccountsBean ab = null;
+		try (
+				Connection con = DbUtil.open();
+				PreparedStatement stmt = con.prepareStatement(sql);) {
+			stmt.setString(1, account_name);
 			ResultSet rs = stmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				ab = new AccountsBean(
 						rs.getInt("account_id"),
 						rs.getString("name"),
@@ -70,43 +67,41 @@ public class SalesServices {
 						rs.getString("password"),
 						rs.getInt("authority"));
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return ab.getAccount_id();
 	}
-	
-	public ArrayList<String> getStaffName(){
-		String sql= "SELECT name FROM accounts";
+
+	public ArrayList<String> getStaffName() {
+		String sql = "SELECT name FROM accounts";
 		ArrayList<String> staffs = new ArrayList<>();
-		try(
-				Connection con =DbUtil.open();
-				PreparedStatement stmt = con.prepareStatement(sql);
-				){
+		try (
+				Connection con = DbUtil.open();
+				PreparedStatement stmt = con.prepareStatement(sql);) {
 			ResultSet rs = stmt.executeQuery();
-			while(rs.next()) {
-				String staff =rs.getString("name");
+			while (rs.next()) {
+				String staff = rs.getString("name");
 				staffs.add(staff);
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return staffs;
 	}
 	
-	public void salesInsert(String day, String name,String sales_category,
-			String trade_name,String unit_price,String sales_num,
+	public void salesInsert(String day, String name, String sales_category,
+			String trade_name, String unit_price, String sales_num,
 			String sales_note) {
 		//sql文作成
-		String sql ="INSERT INTO sales (sale_date,account_id,category_id,trade_name,unit_price,sale_number,note)VALUES (?,?,?,?,?,?,?)";
-		try(
+		String sql = "INSERT INTO sales (sale_date,account_id,category_id,trade_name,unit_price,sale_number,note)VALUES (?,?,?,?,?,?,?)";
+		try (
 				//db接続
-				Connection con =DbUtil.open();
+				Connection con = DbUtil.open();
 				//sql文送信
-				PreparedStatement stmt=con.prepareStatement(sql);
-				){
+				PreparedStatement stmt = con.prepareStatement(sql);) {
 			//1個目のに値をセット
-			stmt.setDate(1,  Date.valueOf(day));
+			stmt.setDate(1, Date.valueOf(day));
 			//2個目のに値をセット
 			stmt.setInt(2, this.getAccountId(name));
 			//3個目のに値をセット
@@ -118,180 +113,184 @@ public class SalesServices {
 			//6個目のに値をセット
 			stmt.setInt(6, Integer.parseInt(sales_num));
 			//7個目のに値をセット
-			stmt.setString(7,sales_note);
+			stmt.setString(7, sales_note);
 			//sql文実行
 			int result = stmt.executeUpdate();
 			System.out.println(result);
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void salesDelete(int sale_id) {
 		String sql = "DELETE FROM sales WHERE sale_id=?";
-		try(
+		try (
 				Connection con = DbUtil.open();
-				PreparedStatement stmt = con.prepareStatement(sql);
-				){
-			stmt.setInt(1,sale_id);
+				PreparedStatement stmt = con.prepareStatement(sql);) {
+			stmt.setInt(1, sale_id);
 			int result = stmt.executeUpdate();
 			System.out.println(result);
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void salesEdit(String day,String name,String sale_category,
-			String trade_name,String unit_price,String sale_num,
-			String sale_note,String sale_id) {
+
+	public void salesEdit(String day, String name, String sale_category,
+			String trade_name, String unit_price, String sale_num,
+			String sale_note, String sale_id) {
 		String sql = "UPDATE sales SET sale_date=?,account_id=?,category_id=?,trade_name=?,unit_price=?,sale_number=?,note=? where sale_id=?";
-		try(
+		try (
 				Connection con = DbUtil.open();
-				PreparedStatement stmt = con.prepareStatement(sql);
-				){
-			stmt.setDate(1,Date.valueOf(day));
-			stmt.setInt(2,this.getAccountId(name));
+				PreparedStatement stmt = con.prepareStatement(sql);) {
+			stmt.setDate(1, Date.valueOf(day));
+			stmt.setInt(2, this.getAccountId(name));
 			stmt.setInt(3, this.getCategoryId(sale_category));
-			stmt.setString(4,trade_name);
+			stmt.setString(4, trade_name);
 			stmt.setString(5, unit_price);
 			stmt.setString(6, sale_num);
 			stmt.setString(7, sale_note);
 			stmt.setInt(8, Integer.parseInt(sale_id));
 			int result = stmt.executeUpdate();
 			System.out.println(result);
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public ArrayList<SearchResultBean> searchSales(String min_day, String max_day, String name, String sale_category, String trade_name, String sale_note) {
-	    ArrayList<SearchResultBean> sales = new ArrayList<>();
-	    String sql = "SELECT s.sale_id, s.sale_date, a.name, c.category_name, " +  
-                "s.trade_name AS trade_name, s.sale_number, s.unit_price, s.note " +  
-                "FROM sales s JOIN accounts a ON s.account_id = a.account_id " +  
-                "JOIN categories c ON s.category_id = c.category_id WHERE 1=1 ";
-   
-   // 条件に応じてSQL文を動的に追加
-   if (min_day != null && !min_day.isEmpty() && max_day != null && !max_day.isEmpty()) {
-       sql += "AND s.sale_date BETWEEN ? AND ? ";  
-   }
-   if (!name.equals("0") ) {
-       sql += "AND a.name = ? ";  
-   }
-   if (!sale_category.equals("0")) {
-       sql += "AND c.category_name = ? ";  
-   }
-   if (trade_name != null && !trade_name.isEmpty()) {
-       sql += "AND s.trade_name LIKE ? ";  
-   }
-   if (sale_note != null && !sale_note.isEmpty()) {
-       sql += "AND s.note LIKE ? ";  
-   }
-	    
-	    try (
-	        Connection con = DbUtil.open();
-	        PreparedStatement stmt = con.prepareStatement(sql);
-	    ) {
-	        // ?の位置の変数
-	        int paramIndex = 1;
-	        if (min_day != null && !min_day.isEmpty() && max_day != null && !max_day.isEmpty()) {
-	            stmt.setString(paramIndex++, min_day);
-	            stmt.setString(paramIndex++, max_day);
-	        }
-	        if (!name.equals("0")) {
-	            stmt.setString(paramIndex++, name);
-	        }
-	        if (!sale_category.equals("0")) {
-	            stmt.setString(paramIndex++, sale_category);
-	        }
-	        if (trade_name != null && !trade_name.isEmpty()) {
-	            stmt.setString(paramIndex++, "%" + trade_name + "%");
-	        }
-	        if (sale_note != null && !sale_note.isEmpty()) {
-	            stmt.setString(paramIndex++, "%" + sale_note + "%");
-	        }
 
-	        ResultSet rs = stmt.executeQuery();
-	        
-	        while (rs.next()) {
-	            int sale_id = rs.getInt("sale_id");
-	            Date sale_date = rs.getDate("sale_date");
-	            String staff = rs.getString("name");
-	            String c_name = rs.getString("category_name");
-	            String t_name = rs.getString("trade_name");
-	            int sale_num = rs.getInt("sale_number");
-	            int sale_price = rs.getInt("unit_price");
-	            int subtotal = sale_num * sale_price;
-	            String note = rs.getString("note");
+	public ArrayList<SearchResultBean> searchSales(String min_day, String max_day, String name, String sale_category,
+			String trade_name, String sale_note) {
+		ArrayList<SearchResultBean> sales = new ArrayList<>();
+		String sql = "SELECT s.sale_id, s.sale_date, a.name, c.category_name, " +
+				"s.trade_name AS trade_name, s.sale_number, s.unit_price, s.note " +
+				"FROM sales s JOIN accounts a ON s.account_id = a.account_id " +
+				"JOIN categories c ON s.category_id = c.category_id WHERE 1=1 ";
 
-	            SearchResultBean srb = new SearchResultBean(sale_id, sale_date, staff, c_name, t_name, sale_num, sale_price, subtotal,note);
-	            sales.add(srb);
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
+		// 条件に応じてSQL文を動的に追加
+		if (min_day != null && !min_day.isEmpty() && max_day != null && !max_day.isEmpty()) {
+			sql += "AND s.sale_date BETWEEN ? AND ? ";
+		}
+		if (!name.equals("0")) {
+			sql += "AND a.name = ? ";
+		}
+		if (!sale_category.equals("0")) {
+			sql += "AND c.category_name = ? ";
+		}
+		if (trade_name != null && !trade_name.isEmpty()) {
+			sql += "AND s.trade_name LIKE ? ";
+		}
+		if (sale_note != null && !sale_note.isEmpty()) {
+			sql += "AND s.note LIKE ? ";
+		}
 
-	    return sales;
+		try (
+				Connection con = DbUtil.open();
+				PreparedStatement stmt = con.prepareStatement(sql);) {
+			// ?の位置の変数
+			int paramIndex = 1;
+			if (min_day != null && !min_day.isEmpty() && max_day != null && !max_day.isEmpty()) {
+				stmt.setString(paramIndex++, min_day);
+				stmt.setString(paramIndex++, max_day);
+			}
+			if (!name.equals("0")) {
+				stmt.setString(paramIndex++, name);
+			}
+			if (!sale_category.equals("0")) {
+				stmt.setString(paramIndex++, sale_category);
+			}
+			if (trade_name != null && !trade_name.isEmpty()) {
+				stmt.setString(paramIndex++, "%" + trade_name + "%");
+			}
+			if (sale_note != null && !sale_note.isEmpty()) {
+				stmt.setString(paramIndex++, "%" + sale_note + "%");
+			}
+
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				int sale_id = rs.getInt("sale_id");
+				Date sale_date = rs.getDate("sale_date");
+				String staff = rs.getString("name");
+				String c_name = rs.getString("category_name");
+				String t_name = rs.getString("trade_name");
+				int sale_num = rs.getInt("sale_number");
+				int sale_price = rs.getInt("unit_price");
+				BigInteger subtotal = BigInteger.valueOf(sale_num).multiply(BigInteger.valueOf(sale_price));
+				String note = rs.getString("note");
+
+				SearchResultBean srb = new SearchResultBean(sale_id, sale_date, staff, c_name, t_name, sale_num,
+						sale_price, subtotal, note);
+				sales.add(srb);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return sales;
 	}
-	public BigInteger subTotal(String sale_num,String unit_price) {
-		BigInteger  num =new BigInteger(sale_num);
+
+	public BigInteger subTotal(String sale_num, String unit_price) {
+		BigInteger num = new BigInteger(sale_num);
 		BigInteger price = new BigInteger(unit_price);
 		BigInteger subtotal = num.multiply(price);
 		return subtotal;
 	}
-	
+
 	public boolean selectCheck(String select) {
 		boolean f = false;
-		if(select.equals("0")) {
+		if (select.equals("0")) {
 			f = false;
-		}else {
-			f=true;
+		} else {
+			f = true;
 		}
 		return f;
 	}
-	
+
 	public boolean tradeNameCheck(String trade_name) {
 		boolean f = false;
-		if(trade_name.length()>100) {
-			f=false;
-		}else {
-			f=true;
+		if (trade_name.length() > 100) {
+			f = false;
+		} else {
+			f = true;
 		}
 		return f;
 	}
+
 	public boolean noteCheck(String note) {
 		boolean f = false;
-		if(note.length()>400) {
-			f=false;
-		}else {
-			f=true;
+		if (note.length() > 400) {
+			f = false;
+		} else {
+			f = true;
 		}
 		return f;
 	}
+
 	public int saleNumCheck(String num) {
 		int f = 0;
-		try{
-			if(Integer.parseInt(num)>2147483647) {
-				f=1;//１なら長さエラー
-			}else {
+		try {
+			if (Integer.parseInt(num) > 2147483647) {
+				f = 1;//１なら長さエラー
+			} else {
 				int sale_num = Integer.parseInt(num);
-				f=3;//３なら問題なし
+				f = 3;//３なら問題なし
 			}
-		}catch(Exception e) {
-			f=2;//２ならフォーマットエラー
+		} catch (Exception e) {
+			f = 2;//２ならフォーマットエラー
 		}
 		return f;
-	}public int salePriceCheck(String price) {
+	}
+
+	public int salePriceCheck(String price) {
 		int f = 0;
-		try{
-			if(Integer.parseInt(price)>2147483647) {
-				f=1;//１なら長さエラー
-			}else {
-				int sale_price =Integer.parseInt(price);
-				f=3;//３なら問題なし
+		try {
+			if (Integer.parseInt(price) > 2147483647) {
+				f = 1;//１なら長さエラー
+			} else {
+				int sale_price = Integer.parseInt(price);
+				f = 3;//３なら問題なし
 			}
-		}catch(Exception e) {
-			f=2;//２ならフォーマットエラー
+		} catch (Exception e) {
+			f = 2;//２ならフォーマットエラー
 		}
 		return f;
 	}
