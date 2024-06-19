4d2e8102cc3b74753469d82a7703b80dfef6d9c4
User <User@WINDOWS-9201UDC>
2024-06-19 14:32:39 +0900

    ログインした状態でAccountRegisterComfirmに直接行こうとすると、accountRegisterに遷移するようにしました

--- PBL_04/src/main/java/controller/AccountRegisterConfirm.java
+++ PBL_04/src/main/java/controller/AccountRegisterConfirm.java
@@ -30,7 +30,7 @@ public class AccountRegisterConfirm extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("S0030");
 	}
 
 	/**
